a7e5e85ea6a6367d4e8f34fa3d1a0f72098e38bc
Henning Buhl <henning.buhl@yahoo.de>
2022-09-29 12:14:37 +0200

    Added arguments class with TODOs

--- src/arguments.py
+++ src/arguments.py
@@ -1,2 +1,7 @@
 # TODO argument names, default values, help strings
 # TODO arguments that are not used in argparse but shared in notebooks (e.g. save_top_k or )
+
+
+# TODO
+def sanity_check_args(args):
+  pass
