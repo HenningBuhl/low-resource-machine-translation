25fb6c66e21c7e2b2e0eac1970ddbdb7ce017068
Henning Buhl <henning.buhl@yahoo.de>
2022-09-30 18:55:25 +0200

    Added eval-before-train arg

--- src/plotting.py
+++ src/plotting.py
@@ -4,20 +4,23 @@ import numpy as np
 
 def plot_metric(metrics, metric, save_path=None):
     '''Plot (and save) a metric that has been recorded during training, validation and testing.'''
-    epochs = len(metrics[f'train_{metric}_epoch'])
+    train_epochs = len(metrics[f'train_{metric}_epoch'])
+    val_epochs = len(metrics[f'train_{metric}_epoch'])
     batches = len(metrics[f'train_{metric}_step'])
-    epoch_steps = np.linspace(1, epochs, epochs)
-    batch_steps = np.linspace(1, epochs, batches)
+    val_offset = val_epochs - train_epochs
+    val_epoch_steps = np.linspace(1 - val_offset, val_epochs, val_epochs)
+    train_epoch_steps = np.linspace(1, train_epochs, train_epochs)
+    batch_steps = np.linspace(1, train_epochs, batches)
 
     plt.plot(batch_steps, metrics[f'train_{metric}_step'], linestyle='-', marker='', color='blue', label=f'train {metric} (batches)')
-    plt.plot(epoch_steps, metrics[f'train_{metric}_epoch'], linestyle='-', marker='.', color='red', label=f'train {metric} (epochs)')
-    plt.plot(epoch_steps, metrics[f'val_{metric}_epoch'], linestyle='-', marker='.', color='orange', label=f'val {metric} (epochs)')
-    plt.plot([epochs], [metrics[f'test_{metric}_epoch'][-1]], linestyle='', marker='o', markersize=3, color='cyan', label=f'test {metric}')
+    plt.plot(train_epoch_steps, metrics[f'train_{metric}_epoch'], linestyle='-', marker='.', color='red', label=f'train {metric} (epochs)')
+    plt.plot(val_epoch_steps, metrics[f'val_{metric}_epoch'], linestyle='-', marker='.', color='orange', label=f'val {metric} (epochs)')
+    plt.plot([train_epochs], [metrics[f'test_{metric}_epoch'][-1]], linestyle='', marker='o', markersize=3, color='cyan', label=f'test {metric}')
 
     plt.title(metric)
     plt.xlabel('epochs')
     plt.ylabel(metric)
-    plt.xticks(epoch_steps)
+    plt.xticks(val_epoch_steps)
     plt.legend(loc='best')
 
     if save_path is not None:
