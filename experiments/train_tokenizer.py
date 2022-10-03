7c2fb680db5211813dfe9d0c821e201e858a6876
Henning Buhl <henning.buhl@yahoo.de>
2022-10-03 22:16:07 +0200

    Create tokenizer dir before training

--- experiments/train_tokenizer.py
+++ experiments/train_tokenizer.py
@@ -27,7 +27,6 @@ def main():
     # Experiment.
     parser.add_argument('--lang', default='de', type=str, help='The tokenizer language.')
     parser.add_argument('--fresh-run', default=False, type=strtobool, help='Ignores all cashed data on disk, reruns generation and overwrites everything.')
-    #parser.add_argument('--seed', default=0, type=int, help='The random seed of the program.')
     parser.add_argument('--vocab-size', default=16000, type=int, help='The vocabulary size of the tokenizer.')
     parser.add_argument('--char-coverage', default=1.0, type=float, help='The character coverage (percentage) of the tokenizer.')
 
@@ -37,6 +36,9 @@ def main():
     # Print args.
     print(f'Arguments:')
     print(args)
+
+    # Create tokenizer dir.
+    create_dir(CONST_TOKENIZERS_DIR)
 
     # Train tokenizer.
     TokenizerBuilder(args.lang).build(args.vocab_size, args.char_coverage, fresh_run=args.fresh_run)
